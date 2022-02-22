944766d7d60b916aeed82d1497166f3e7ee98897
shaik325 <shaikbabavali515@gmail.com>
2022-02-22 12:31:38 +0530

    shellscript files

---
+++ commandlinearg.sh
@@ -0,0 +1,9 @@
+ hello
+
+ welcome to command line arguments in the shellscript
+
+
+ main purpose of the commaand line arguments is the  passing the arguments at the time of execution
+
+ exit\
+
---
+++ variables.sh
@@ -0,0 +1,13 @@
+hello
+
+welcome to variables in the shellscript
+
+
+there are two types of the variables in the shellscript 
+
+
+1.system defined variables
+
+2.user defined variables
+
+

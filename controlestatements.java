395e1299b87f249ab586df3d27a6e264df71dccb
shaik325 <shaikbabavali515@gmail.com>
2022-02-22 12:29:11 +0530

    java files

---
+++ controlestatements.java
@@ -0,0 +1,15 @@
+hello
+
+weelcome to controle statements in java
+
+
+if  statements
+
+for loop  statements
+
+while loop  statements
+
+
+switch case statements
+
+
---
+++ inheritance.java
@@ -0,0 +1,7 @@
+hello
+
+welcome to java concept
+
+inheritence is the one of yhe important concept in java
+
+
